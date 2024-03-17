b58bb14c5ae8f4e82cc811dd7bfe7b1e89aa5cbc
Jhonatan Reis <reisdeveloper10@gmail.com>
2024-03-17 11:52:45 -0300

    estrutura created

---
+++ lib/core/presetation/screens/home/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:app_clima/core/presetation/screens/loading/loading_screen.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoadingScreen(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:app_clima/core/presetation/screens/home/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const HomeScreen());

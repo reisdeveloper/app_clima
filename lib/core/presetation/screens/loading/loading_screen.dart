3ac42880ba938ac285d32e5dad5a81841e99d346
Jhonatan Reis <reisdeveloper10@gmail.com>
2024-03-17 23:24:28 -0300

    metodo para localizcao criado

--- lib/core/presetation/screens/loading/loading_screen.dart
+++ lib/core/presetation/screens/loading/loading_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+import '../../../config/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -8,15 +11,48 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  @override
+  void initState() {
+   super.initState();
+   getLocation();
+  }
+
+  Future<void> getLocation() async {
+    Location location = Location();
+    try {
+      await location.getCurrentLocation();
+      setState(() {
+       print("Longitude do usuário: ${location.longitude}");
+       print("Lagitude do usuário: ${location.latitude}");
+       print("Localização do usuário: ${location.longitude} ${location.latitude}");
+
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {},
-          child: const Text("Get Location"),
+      appBar: AppBar(title: const Text("Location Page")),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  getLocation();
+                },
+                child: const Text("Mostre-me minha posição"),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+
+
 }
